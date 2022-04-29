d6d20b6015b387862b1f17ae43cdd6a7c3b1162b
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-30 00:30:43 +0530

    Get

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import os
 from pyrogram import Client, idle
 
-API_ID = os.environ('API_ID', '')
-API_HASH = os.environ('API_HASH', '') 
-SESSION = os.environ('SESSION', '')
-BOT_TOKEN = os.environ('BOT_TOKEN', '')
+API_ID = os.environ.get('API_ID', '')
+API_HASH = os.environ.get('API_HASH', '') 
+SESSION = os.environ.get('SESSION', '')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 
 bot = Client("recallbot", API_ID, API_HASH, bot_token=BOT_TOKEN)
 user = Client(SESSION, API_ID, API_HASH) 
