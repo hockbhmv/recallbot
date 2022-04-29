efc98a2e9734d4a455d150bcf75e81d4fc448ebf
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-30 01:04:46 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+import os 
+import pyrogram Client 
+
+API_ID = os.environ.get('API_ID', '')
+API_HASH = os.environ.get('API_HASH', '') 
+SESSION = os.environ.get('SESSION', '')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+
+bot = Client("recallbot", API_ID, API_HASH, plugins={"root": "run"}, bot_token=BOT_TOKEN)
+user = Client(SESSION, API_ID, API_HASH) 
