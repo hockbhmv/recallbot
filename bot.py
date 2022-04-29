86b7957172c36598465bc3a7d7b3bf22e520e91d
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-30 00:45:03 +0530

    run

--- bot.py
+++ bot.py
@@ -1,19 +1,23 @@
 import os
-from pyrogram import Client, idle
+from pyrogram import Client 
+
+import logging
+import logging.config
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 API_ID = os.environ.get('API_ID', '')
 API_HASH = os.environ.get('API_HASH', '') 
 SESSION = os.environ.get('SESSION', '')
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 
-bot = Client("recallbot", API_ID, API_HASH, bot_token=BOT_TOKEN)
+bot = Client("recallbot", API_ID, API_HASH, plugins={"root": "run"}, bot_token=BOT_TOKEN)
 user = Client(SESSION, API_ID, API_HASH) 
 
-bot.start()
-print('Bot started')
-user.start()
-print('User started')
-idle()
-bot.stop()
-user.stop()
-print('BOTH ARE STOPPED')
+async def start():
+  await user.start()
+  logging.info('userbot started')
+  logging.info('Bot started')
+
+bot.run(start())
