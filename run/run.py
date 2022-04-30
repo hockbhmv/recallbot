8c9ea9728990d109bd16bc399455c87e951aa14a
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-30 05:34:01 +0530

    Try

--- run/run.py
+++ run/run.py
@@ -3,6 +3,7 @@ import json
 import logging
 from config import bot, user 
 from pyrogram import filters
+from pyrogram.errors import InputUserDeactivated, FloodWait, UserIsBlocked, PeerIdInvalid 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR) 
@@ -17,21 +18,40 @@ async def request(client, msg):
       return await msg.reply_text('provide chat id or username')
    chat_id = msg.command[1]
    edit = await msg.reply('.......')
+   FRMT = "{}\n\nsucess: `{}` | `{}`\naccount deleted: `{}`\npeerid invalid: `{}`\nerror occurred: `{}`"
    try:
      group = await user.get_chat(chat_id)
      LIST = []
      total = 0
      pling = 0
+     success = 0
+     peerid = 0
+     deleted = 0
+     error = 0
      async for member in user.iter_chat_members(group.id, filter="all"):
+         try:
+            await msg.reply_to_message.copy(member.user.username or member.user.id) 
+            success += 1
+         except FloodWait as e:
+            asyncio.sleep(e.x)
+            await msg.reply_to_message.copy(member.user.username or member.user.id) 
+            success += 1
+         except InputUserDeactivated:
+            deleted += 1
+         except PeerIdInvalid:
+            peerid += 1
+         except Exception as e:
+            errors += 1
+            print(e)
          LIST.append(f"{member.user.first_name} ({member.user.id}) [@{member.user.username}]")
          total += 1
          pling += 1
          if pling % 10 == 0:
-            await edit.edit(f"completed: {total}")
+            await edit.edit(FRMT.format("BROADCAST PROGRESSING", success, total, deleted, peerid, error)
      with open(f"{msg.from_user.id}.json", "w+") as out:
           json.dump(LIST, out) 
      await client.send_document(msg.chat.id, f"{msg.from_user.id}.json", file_name="MEMBER.json", caption="MEMBERS DETAILS")
      os.remove(f"{msg.from_user.id}.json")
-     await edit.edit(f"completed: {total}")
+     await edit.edit(FRMT.format("BROADCAST COMPLETED", success, total, deleted, peerid, error))
    except Exception as e:
      await msg.reply(e) 
